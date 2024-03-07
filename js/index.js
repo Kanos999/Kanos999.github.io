ab2c765eda4ae6f75d079fdf587e895b297a2bcc
Kane Jackson <kane@kanejackson.com>
2024-03-07 15:55:36 +1100

    removed routes, github papges doesn't support it

--- js/index.js
+++ js/index.js
@@ -1,17 +1,18 @@
-let isMobile = false;
-if (navigator.userAgent.match(/Android/i)
-				|| navigator.userAgent.match(/webOS/i)
-				|| navigator.userAgent.match(/iPhone/i)
-				|| navigator.userAgent.match(/iPad/i)
-				|| navigator.userAgent.match(/iPod/i)
-				|| navigator.userAgent.match(/BlackBerry/i)
-				|| navigator.userAgent.match(/Windows Phone/i)) {
-					isMobile = true ;
-					window.location.replace("./mobile/index.html");
-				}
 
 $( document ).ready(function() {
 	console.log("ready!");
+
+	let isMobile = false;
+	if (navigator.userAgent.match(/Android/i)
+			|| navigator.userAgent.match(/webOS/i)
+			|| navigator.userAgent.match(/iPhone/i)
+			|| navigator.userAgent.match(/iPad/i)
+			|| navigator.userAgent.match(/iPod/i)
+			|| navigator.userAgent.match(/BlackBerry/i)
+			|| navigator.userAgent.match(/Windows Phone/i)) {
+				isMobile = true ;
+				window.location.replace("./mobile/index.html");
+			}
 
 	typeWriter("#landingTitle", "Hi! I'm Kane", 80);
 	setTimeout(function() {
